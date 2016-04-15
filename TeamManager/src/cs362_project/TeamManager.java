7709c61fba703cb532f50498ffafa9dbbdcf57c1
Alex Berns <tagger94@gmail.com>
2016-04-15 04:00:25 +0000

    Alex's Code

--- TeamManager/src/cs362_project/TeamManager.java
+++ TeamManager/src/cs362_project/TeamManager.java
@@ -2,6 +2,48 @@ package cs362_project;
 
 public class TeamManager {
 	DatabaseSupport d = new DatabaseSupport();
+	
+	public boolean createTeam(String name, String tid, String mid) {
+		try {
+
+			Team t = new Team(String name, String tid, String mid);
+
+			d.putTeam(t);
+
+		} catch (Exception e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean deleteTeam(String tid) {
+		try {
+
+			d.deleteTeam(tid);
+
+		} catch (Exception e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean addManager(int mid, int tid) {
+		try {
+
+			Team t = d.getTeam(tid);
+
+			t.addUpdateContactInfoManager();
+
+			d.putTeam(t);
+
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
 
 	public boolean RemoveManager(int mid, int tid) {
 		try {
