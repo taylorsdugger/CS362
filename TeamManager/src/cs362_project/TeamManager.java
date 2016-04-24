cb8833c3ee2208d910589131ebea7b9008bcb09d
Taylor <tdugger@iastate.edu>
2016-04-24 17:41:42 -0500

    re

--- TeamManager/src/cs362_project/TeamManager.java
+++ TeamManager/src/cs362_project/TeamManager.java
@@ -1,12 +1,16 @@
 package cs362_project;
 
+import cs362_project.Member;
+
+import java.util.List;
+
 public class TeamManager {
 	DatabaseSupport d = new DatabaseSupport();
 	
 	public boolean createTeam(int tid, int mid) {
 		try {
 
-			Team t = new Team(int tid, int mid);
+			Team t = new Team( tid,  mid);
 
 			d.putTeam(t);
 
@@ -72,5 +76,44 @@ public class TeamManager {
 		}
 
 		return true;
+	}
+	
+	public boolean addMember(String name, int TiD,int MiD)
+	{
+		try
+		{
+			Member m=new Member(MiD,TiD,name);
+			d.putMemberIntoTeam(m);	
+		}catch(Exception e)
+		{
+			return false;
+			
+		}
+		return true;
+	}
+	public boolean deleteMember(int mid)
+	{
+		
+		try
+		{
+			d.removeMember(mid);
+		}catch(Exception e)
+		{
+			return false;
+			
+		}
+		return true;
+	}
+	public List<Member> listMember(int tID)
+	{	
+		try
+		{
+			return d.getMembers(tID);
+		}catch(Exception e)
+		{
+			return null;
+			
+		}
+		
 	}
 }
